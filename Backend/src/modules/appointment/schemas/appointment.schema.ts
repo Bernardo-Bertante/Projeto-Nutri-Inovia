5135328b0b9e09cf3f00fba9a1760e9e1c2e2174
Bernardo Bertante <bernardo.sctp@gmail.com>
2026-01-29 13:37:57 -0300

    feat: appointment schema and class-validator

---
+++ Backend/src/modules/appointment/schemas/appointment.schema.ts
@@ -0,0 +1,44 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+export type AppointmentDocument = HydratedDocument<Appointment>;
+
+export enum BodyType {
+  ECTOMORPH = 'Ectomorfo',
+  MESOMORPH = 'Mesomorfo',
+  ENDOMORPH = 'Endomorfo',
+}
+
+@Schema({ timestamps: true })
+export class Appointment {
+  @Prop({ required: true })
+  startDate: Date;
+
+  @Prop({ required: true })
+  endDate: Date;
+
+  @Prop({ required: true })
+  nutritionistId: string;
+
+  @Prop({ required: true })
+  patientName: string;
+
+  @Prop({ required: true })
+  email: string;
+
+  @Prop({ required: true })
+  phoneNumber: string;
+
+  @Prop({ required: true })
+  birthDate: Date;
+
+  @Prop({ required: true, enum: BodyType })
+  bodyType: BodyType;
+
+  @Prop({ required: true })
+  cpf: string;
+}
+
+export const AppointmentSchema = SchemaFactory.createForClass(Appointment);
+
+AppointmentSchema.index({ nutritionistId: 1, startDate: 1 });
