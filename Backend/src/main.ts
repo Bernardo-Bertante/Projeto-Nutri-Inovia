adbf7b444803a1158b79a48ed948766be6f45873
Bernardo Bertante <bernardo.sctp@gmail.com>
2026-02-11 18:46:01 -0300

    fix: main.ts addBearerAuth on document builder

--- Backend/src/main.ts
+++ Backend/src/main.ts
@@ -20,6 +20,7 @@ async function bootstrap() {
     .setTitle('Nutri Inovia API')
     .setDescription('API para agendamento de consultas')
     .setVersion('1.0')
+    .addBearerAuth()
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
